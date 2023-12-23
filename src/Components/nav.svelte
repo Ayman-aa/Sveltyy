a497c3d6b1920eb52c6ee36fcfe84049fc1eab69
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2023-12-23 12:04:19 +0100

    Update nav.svelte

--- src/Components/nav.svelte
+++ src/Components/nav.svelte
@@ -11,7 +11,7 @@ function toggleNav() {
   }
 }
  </script>
-<nav class="bg-whitesmoke border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+<nav class="bg-whitesmoke border-gray-200 px-2 sm:px-4 py-2.5 rounded-b-sm rounded-t-none">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <a href="/" class="flex items-center text-2xl font-bold">MovieLib</a>
       <button on:click={toggleNav} data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-full md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
